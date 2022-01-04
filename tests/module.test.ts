1eb736e1ab7e3c3f8cd6359f0f7d105d6429b235
Mgen <m@mgenware.com>
2022-01-04 18:31:19 +0800

    Verify package.json bin field

--- tests/module.test.ts
+++ tests/module.test.ts
@@ -4,3 +4,10 @@ import * as assert from 'assert';
 it('Verify type definition files', async () => {
   assert.ok((await fsPromises.stat('./dist/main.d.ts')).isFile());
 });
+
+it('Verify package.json bin field', async () => {
+  const binObj: unknown = JSON.parse(await fsPromises.readFile('./package.json', 'utf8')).bin;
+  assert.deepStrictEqual(binObj, {
+    dz: './dist/main.js',
+  });
+});
